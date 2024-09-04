351cc93dbb1a61ece6ba4265ee70fd76a7bc72b1
RazaqFarhanR <aljawadfarhan@gmail.com>
2024-09-04 16:24:54 +0700

    feat: add upload image course

--- src/controllers/course.controller.js
+++ src/controllers/course.controller.js
@@ -1,6 +1,7 @@
 const models = require("../models/index")
 const Course = models.course
 const Response = require("../helpers/response")
+const deleteImageFromCloudinary = require("../helpers/deleteImage")
 require('dotenv').config()
 
 module.exports = {
@@ -36,6 +37,32 @@ module.exports = {
             const result = await Course.create(data)
             return Response.addResponse(req, res, result)
         } catch (error) {
+            return Response.errorResponse(req, res, error.message)
+        }
+    },
+
+    uploadCoursePicture: async (req, res) => {
+        try {
+            const param = {id: req.params.id}
+            if (!req.file) {
+                return res.status(400).json({ message: 'No image uploaded' });
+            }
+            const courses = await Course.findOne({where: param})
+            if (courses.imageId) {
+                await deleteImageFromCloudinary(courses.imageId)
+            }
+
+            let data = {
+                imageId: req.file.filename,
+                imageUrl: req.file.path
+            }
+
+            const result = await Course.update(data, {where: param})
+            Response.editResponse(req, res, result)
+        } catch (error) {
+            if (req.file) {
+                await deleteImageFromCloudinary(req.file.filename)
+            }
             return Response.errorResponse(req, res, error.message)
         }
     },
--- src/routes/course.js
+++ src/routes/course.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const courseController = require('../controllers/course.controller');
+const upload = require('../middleware/uploadImage');
 
 const router = express.Router();
 
@@ -7,6 +8,7 @@ const router = express.Router();
 // Course routes
 //= ===============================
 router.post('/', courseController.addCourse);
+router.post('/uploadcover/:id', upload.single('image'), courseController.uploadCoursePicture);
 router.put('/:id', courseController.updateCourse);
 router.delete('/:id', courseController.deleteCourse);
 
