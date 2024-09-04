1d81ff42992e705602743060b2f94f53ecf09c5a
RazaqFarhanR <aljawadfarhan@gmail.com>
2024-09-04 15:33:58 +0700

    feat: add route

--- server.js
+++ server.js
@@ -5,12 +5,23 @@ const cors = require("cors");
 require('dotenv').config();
 require('./src/config/sequelize');
 
+const publicRoute = require('./src/routes/public');
+const adminRoute = require('./src/routes/admin');
+const studentRoute = require('./src/routes/student');
+const courseRoute = require('./src/routes/course');
+const authUser = require("./src/middleware/authUser");
+
 // Set the port number
 const PORT = process.env.PORT || 3000;
 
 app.use(cors())
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
+
+app.use('/api', publicRoute)
+app.use('/api/admin', authUser, adminRoute)
+app.use('/api/student', authUser, studentRoute)
+app.use('/api/course', authUser, courseRoute)
 
 app.listen(PORT,() =>{
   console.log('server run on port ' + PORT)
